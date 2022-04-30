1c86f54e9cf7ebd505f7407cb125d7846f012a43
NuoC <nuoc@kth.se>
2022-04-30 16:14:01 +0200

    Merged with Latest head

--- lua/plugins/configs/orgmode.lua
+++ lua/plugins/configs/orgmode.lua
@@ -1,17 +1,17 @@
 require("orgmode").setup_ts_grammar()
 
--- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
--- parser_config.org = {
---   install_info = {
---     url = 'https://github.com/milisims/tree-sitter-org',
---     revision = 'main',
---     files = {'src/parser.c', 'src/scanner.cc'},
---   },
---   filetype = 'org'
--- }
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'main',
+    files = {'src/parser.c', 'src/scanner.cc'},
+  },
+  filetype = 'org'
+}
 
 require('orgmode').setup({
-  org_agenda_files = {'E:/dev/dice-log/Org/Home', 'E:/dev/dice-log/**/*'},
-  org_default_notes_file = 'E:/dev/dice-log/Org/Home/refile.org',
+  org_agenda_files = {'~/dev/dice-log/Org/Laptop', '~/dev/dice-log/Org/**/*'},
+  org_default_notes_file = '~/dev/dice-log/Org/Laptop/refile.org',
   org_todo_keywords = {'TODO', 'PENDING', '|', 'DONE'},
 })
